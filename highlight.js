9fd88258a448935c2d55fcfb9337f681bb0a02fb
joe.wu <traveller_wjoe@sina.com>
2016-11-05 23:27:25 +0800

    (v0.3)[append]支持搜索框按回车，前进后退时触发高亮

--- highlight.js
+++ highlight.js
@@ -35,7 +35,7 @@ Highlight.prototype = {
         if (aEl.length) {
             for (var i = 0; i < aEl.length; i++) {
                 var text = aEl[i].innerText || aEl[i].textContent,
-                    pattern = new RegExp(keywords.join('|'),'gim'),
+                    pattern = new RegExp(keywords.join('|'), 'gim'),
                     flag = pattern.test(text),
                     html;
                 if (flag) {
@@ -46,9 +46,9 @@ Highlight.prototype = {
         }
     },
     start: function () {
-        this.readyDom=document.createElement('input');
-        this.readyDom.setAttribute('type','hidden');
-        this.readyDom.setAttribute('id','highlightReady');
+        this.readyDom = document.createElement('input');
+        this.readyDom.setAttribute('type', 'hidden');
+        this.readyDom.setAttribute('id', 'highlightReady');
         //等待元素在异步动态添加后执行后续操作
         var s = setInterval(function () {
             var ires = document.getElementById('ires');
@@ -62,20 +62,63 @@ Highlight.prototype = {
     },
     bind: function () {
         var searchBtn = document.querySelector('button[name=btnG][type=submit]'),
+            searchIpt=document.getElementById('lst-ib'),
             _self = this;
-        searchBtn.addEventListener('click', function (e) {
-            var s=setInterval(function () {
+
+        //点击搜索按钮触发高亮
+        searchBtn.addEventListener('click', doHighlight);
+
+        //搜索框enter搜索触发
+        searchIpt.addEventListener('keydown',function (e) {
+            if(e&&e.keyCode==13){
+                doHighlight(e);
+            }
+        })
+
+        //前进后退触发高亮
+        window.addEventListener('popstate',doHighlight);
+
+        //点击超链接搜索触发高亮
+        var rhs=document.getElementById('rhs');
+        var s=setInterval(function() {
+            if (rhs) {
+                bindTagA();
+                clearInterval(s);
+            }
+        },200)
+        function bindTagA() {
+            var aEls = document.querySelectorAll('a:not([onmousedown])');//除去搜索结果标题（跳转到其他页面的a标签）的所有超链接a标签
+            for (var i = 0; i < aEls.length; i++) {
+                var aEl = aEls[i];
+                aEl.addEventListener('click', doHighlight)
+            }
+        }
+
+
+
+        function doHighlight(e) {
+            var s = setInterval(function () {
                 var ires = document.getElementById('ires');
                 console.log(ires);
-                var readyEl=ires.querySelector('#highlightReady');
-                if(!readyEl){
+                var readyEl = ires.querySelector('#highlightReady');
+                if (!readyEl) {
                     ires.appendChild(_self.readyDom);
                     clearInterval(s);
+
+                    var rhs=document.getElementById('rhs');
+                    var s2=setInterval(function() {
+                        if (rhs) {
+                            bindTagA();
+                            clearInterval(s2);
+                        }
+                    },200)
+                    // bindTagA();
                     _self.highlight();
                 }
-            },200);
-        })
+            }, 200);
+        }
     }
+
     //前进后退不支持，点击搜索结果中的超链接异步加载不支持
 }
 
